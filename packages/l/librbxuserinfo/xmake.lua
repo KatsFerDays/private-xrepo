be848136da13ce1b2ef3891f0cbc1a0827c571be
KatsFerDays <N/A>
2022-10-23 16:15:27 -0400

    e

--- packages/l/librbxuserinfo/xmake.lua
+++ packages/l/librbxuserinfo/xmake.lua
@@ -2,7 +2,7 @@ package("librbxuserinfo")
     set_description("The libRbxUserInfo package")
 
     add_urls("https://github.com/KatsFerDays/libRbxUserInfo.git")
-    add_versions("1.0.0", "9bbdbb01f39df486e287fc2d836e46d2b20e3bf6")
+    add_versions("1.0.0", "0014851b1ac31ea56566f090b6b12aef38e356ab")
 
     on_install(function (package)
         local configs = {}
