f4a1bde94d8bc2cab92d281ee15ea33f1a47ba88
KatsFerDays <N/A>
2022-10-23 00:39:35 -0400

    the fitness gram pacer test is

--- packages/l/librbxuserinfo/xmake.lua
+++ packages/l/librbxuserinfo/xmake.lua
@@ -10,7 +10,7 @@ package("librbxuserinfo")
             configs.kind = "shared"
         end
         import("package.tools.xmake").install(package, configs)
-        os.cp("include/*", package:installdir())
+        os.cp("*.hpp", package:installdir("include"))
     end)
 
     on_test(function (package)
