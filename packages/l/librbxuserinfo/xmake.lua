41c5fb18638c217ee777bf3ca401827f0429a5be
KatsFerDays <N/A>
2022-10-22 23:11:55 -0400

    copy include

--- packages/l/librbxuserinfo/xmake.lua
+++ packages/l/librbxuserinfo/xmake.lua
@@ -2,13 +2,14 @@ package("librbxuserinfo")
     set_description("The libRbxUserInfo package")
 
     add_urls("https://github.com/KatsFerDays/libRbxUserInfo.git")
-    add_versions("1.0.0", "8f77daeb80bbfab1bf16464e25609f0b81725e51")
+    add_versions("1.0.0", "5dcc40c91be6a4dcfd2dcdd961e53ac4756e5863")
 
     on_install(function (package)
         local configs = {}
         if package:config("shared") then
             configs.kind = "shared"
         end
+        os.cp("include", package:installdir())
         import("package.tools.xmake").install(package, configs)
     end)
 
